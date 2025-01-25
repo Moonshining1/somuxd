83c0e4372ad4369e7a48324f94849a4f3bf363bd
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2025-01-25 10:38:52 +0530

    Update start.py

--- AnieXEricaMusic/plugins/bot/start.py
+++ AnieXEricaMusic/plugins/bot/start.py
@@ -29,14 +29,14 @@ from strings import get_string
 #--------------------------
 
 NEXI_VID = [
-"https://files.catbox.moe/rp5ai8.mp4",
-"https://files.catbox.moe/yh19xh.mp4",
-"https://files.catbox.moe/6t7n6u.mp4",
-"https://files.catbox.moe/tc7tjs.mp4",
-"https://files.catbox.moe/4p5os4.mp4",
-"https://files.catbox.moe/58c1cj.mp4",
-"https://files.catbox.moe/wh9pgk.mp4",
-"https://files.catbox.moe/vh49fu.mp4",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
+"https://telegra.ph/file/c3e75a195cdda1380a176.jpg",
 
 ]
 
@@ -48,7 +48,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_video(
+            return await message.reply_photo(
                 random.choice(NEXI_VID),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
@@ -104,7 +104,7 @@ async def start_pm(client, message: Message, _):
         await baby.edit_text(f"**❖ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ..**")
         await baby.delete()
         
-        await message.reply_video(
+        await message.reply_photo(
             random.choice(NEXI_VID),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
@@ -121,7 +121,7 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_video(
+    await message.reply_photo(
         random.choice(NEXI_VID),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
@@ -156,7 +156,7 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_video(
+                await message.reply_photo(
                     random.choice(NEXI_VID),
                     caption=_["start_3"].format(
                         message.from_user.mention,
